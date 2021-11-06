9f5cf7d47ae9b3d432a5d0b17b96dbf827911122
a.gulzadian@gmail.com <a.gulzadian@gmail.com>
2021-11-06 13:36:15 +0100

    setup for run with new scheduler approach ready

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import * as schedule from "node-schedule";
 import { WebsocketClient, WsKey, WsResponse, WsUserDataEvents } from "binance";
 
 import CronHelper from './helpers/cronHelper';
-import { LogLevel } from './models/log-level'; // todo aram check if loglevel can be included in txtLogger class
 import Tradingbot from './tradingbot';
 import WebSocket from 'isomorphic-ws';
 import WebSocketService from './binance-service/websocket';
@@ -13,8 +12,8 @@ import txtLogger from './helpers/txt-logger';
 
 export default class Main { // todo aram this wrapper is kind of uselss I think, can do all of this stuff directly in index.ts as well, maybe just for tidyness use this wrapper
     private tradingBot = new Tradingbot();
-    private scheduledJob: schedule.Job = new schedule.Job(async function () {
-        await tradingBot.runProgram();
+    private job: schedule.Job = new schedule.Job(async function () {
+        await this.tradingBot.runProgram();
     });
     
     public async Start() {
@@ -27,10 +26,7 @@ export default class Main { // todo aram this wrapper is kind of uselss I think,
 
         txtLogger.log('Subscribing to webSocketClient');
         
-        //websocketClient.subscribeSpotUserDataStream();
-
-        this.scheduledJob.schedule(cronExpression);
-        this.scheduledJob.cancel();
+        websocketClient.subscribeSpotUserDataStream();
 
         // Retreive some config values
         const runTestInsteadOfProgram: boolean = config.production.devTest.triggerBuyOrderLogic;
@@ -43,11 +39,9 @@ export default class Main { // todo aram this wrapper is kind of uselss I think,
             txtLogger.log(`Websocket event - connection opened:', ${data.wsKey}, ${data.ws.url}`);
 
             if (runTestInsteadOfProgram === false) {
-                this.scheduledJob = schedule.scheduleJob(cronExpression, async function () {
-                    await tradingBot.runProgram();
-                });
+                this.job.schedule(cronExpression);
             } else {
-                await tradingBot.runProgram();
+                this.job.invoke();
             }
 
             // wsService.requestListSubscriptions(websocketClient, data.wsKey, 1);
@@ -61,19 +55,19 @@ export default class Main { // todo aram this wrapper is kind of uselss I think,
         // Listen To Order Changes
         websocketClient.on('formattedUserDataMessage', async (data: WsUserDataEvents) => {
             txtLogger.log(`formattedUserDataMessage eventreceived: ${JSON.stringify(data)}`);
-            await tradingBot.processFormattedUserDataMessage(data);
+            await this.tradingBot.processFormattedUserDataMessage(data);
         });
     }
 
     public  async Stop() {
         txtLogger.log('Stopping scheduled job');
 
-        var jobCancelled = schedule.cancelJob(this.scheduledJob);
+        var jobCancelled = this.job.cancel();
 
         if (jobCancelled) {
             txtLogger.log('Job cancelled');
         } else {
-            txtLogger.log('Warning, unable to cancel job'); // todo aram maube we want an email to be sent out in this case?
+            txtLogger.log('Warning, unable to cancel job'); // todo aram maybe we want an email to be sent out in this case?
         }
     }
 }
